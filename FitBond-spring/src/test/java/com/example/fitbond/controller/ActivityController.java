26d8f792363c7febbc2ee9ff4fa2a9865b268f3b
Giamcaaa <giandiaz29and@gmail.com>
2026-04-13 14:00:21 +0200

    ActivityController.java

--- FitBond-spring/src/test/java/com/example/fitbond/controller/ActivityController.java
+++ FitBond-spring/src/test/java/com/example/fitbond/controller/ActivityController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controller;
-
 import com.example.demo.Activity;
 import com.example.demo.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,22 +14,20 @@ public class ActivityController {
 
     @Autowired
     private ActivityService activityService;
-
     @PostMapping
     public ResponseEntity<Activity> createActivity(
             @RequestBody Activity activity,
+            @RequestParam int userId,
             @RequestParam double latitude,
             @RequestParam double longitude) {
 
-        Activity saved = activityService.createActivity(activity, latitude, longitude);
+        Activity saved = activityService.createActivity(activity, userId, latitude, longitude);
         return ResponseEntity.ok(saved);
     }
-
     @GetMapping("/user/{userId}")
     public List<Activity> getUserActivities(@PathVariable int userId) {
         return activityService.getActivitiesByUserId(userId);
     }
-
     @GetMapping("/{id}")
     public ResponseEntity<Activity> getActivity(@PathVariable int id) {
         return activityService.getActivityById(id)
